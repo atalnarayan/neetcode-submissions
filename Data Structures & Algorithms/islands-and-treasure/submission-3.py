02156bd17692876df76770f9a680d87fa52fdc11
Atal Narayan Sahu <37247664+atalnarayan@users.noreply.github.com>
2026-07-13 17:21:15 -0700

    Add: islands-and-treasure - submission-3

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        m, n = len(grid), len(grid[0])
+        INF = 2147483647
+
+        def bfs(queue):
+            while queue:
+                (i,j) = queue.popleft()
+                for (x,y) in [(i-1, j), (i+1,j), (i,j-1), (i,j+1)]:
+                    if 0<=x<m and 0<=y<n and grid[x][y] == INF:
+                        grid[x][y] = 1 + grid[i][j]
+                        queue.append((x,y))
+
+        queue = deque([])
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == 0:
+                    queue.append((i,j))
+        bfs(queue)
